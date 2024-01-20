72d1159cdd0e030aec32c4cf08ce9902b05935b8
Sc~arlet <105559012+Mergery@users.noreply.github.com>
2024-01-20 13:50:24 +0300

    Test Version Switching
    -Changes to scene
    - Changes to Material
    - Changes to Scripts

---
+++ Assets/Scripts/ToriMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToriMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //hiuehhdwkhnxkf  wihskjhfihihhgjkdksjhishsdkhsdhsjhsfjdfjhsdhf 
+        //TESTING SWITCHING BETWEEN UNITY VERSIONS 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
